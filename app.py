3aa04808c3707c5f6490e03d8dfc41cfa3cf5054
LRVT <21357789+l4rm4nd@users.noreply.github.com>
2025-02-20 15:35:07 +0100

    fix: disable debug mode

--- app.py
+++ app.py
@@ -29,4 +29,4 @@ def calculate_cvss():
         return jsonify({"error": str(e)}), 400
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
