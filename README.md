96a238b65b8625c96b4a1a8f9c99fc9884ca4f51
LRVT <21357789+l4rm4nd@users.noreply.github.com>
2025-02-20 15:03:58 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # CVSS-API
+
 Simple Python Flask API server that returns CVSS scores for CVSS vector strings
+
+## 🐳 Usage
+
+You can use the provided `docker-compose.yml` or just run:
+
+````
+docker run -it -rm -p 5000:5000 ghcr.io/l4rm4nd/cvss-api:latest
+````
+
+Afterwards, you can utilize the API server:
+
+````
+curl "http://127.0.0.1:5000/cvss?vector=CVSS:2.0/AV:N/AC:L/Au:N/C:C/I:C/A:C"
+curl "http://127.0.0.1:5000/cvss?vector=CVSS:3.1/AV:N/AC:L/PR:N/UI:N/S:U/C:H/I:H/A:H"
+curl "http://127.0.0.1:5000/cvss?vector=CVSS:4.0/AV:N/AC:L/AT:N/PR:N/UI:N/VC:H/VI:H/VA:H/SC:N/SI:N/SA:N"
+````
+
+The API will respond with the following JSON:
+
+````
+{
+  "api_version": "0.1.0",
+  "cvss_score": 9.8,
+  "cvss_vector": "CVSS:3.1/AV:N/AC:L/PR:N/UI:N/S:U/C:H/I:H/A:H"
+}
+````
+
+Combine with an TLS reverse proxy. CORS is already allowed on the Flask application.
---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS  # Import CORS
+from cvss import CVSS2, CVSS3, CVSS4
+import os
+
+app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes and origins
+
+@app.route('/cvss', methods=['GET'])
+def calculate_cvss():
+    vector = request.args.get('vector', '')
+    version = os.getenv('SECRET_KEY', 'unknown')
+
+    if not vector:
+        return jsonify({"error": "No CVSS vector provided"}), 400
+
+    try:
+        if vector.startswith("CVSS:2."):
+            score = CVSS2(vector).scores()[0]  # Base Score
+        elif vector.startswith("CVSS:3."):
+            score = CVSS3(vector).scores()[0]  # Base Score
+        elif vector.startswith("CVSS:4."):
+            score = CVSS4(vector).scores()[0]  # Base Score
+        else:
+            return jsonify({"error": "Unsupported or invalsid CVSS version"}), 400
+
+        return jsonify({"cvss_vector": vector, "cvss_score": score, "api_version": version})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
